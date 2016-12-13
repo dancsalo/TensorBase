ab93d1b4c6c3115f326319be8f867b61bec644e9
Dan Salo <dcs41@duke.edu>
2016-12-13 15:48:54 -0500

    Data class tweaks

--- tensorbase/data.py
+++ tensorbase/data.py
@@ -129,3 +129,62 @@ class Mnist(Data):
     def _read32(bytestream):
         dt = np.dtype(np.uint32).newbyteorder('>')
         return np.frombuffer(bytestream.read(4), dtype=dt)[0]
+
+class Bags(Data):
+    def __init__(self, flags):
+        super().__init__(flags)
+
+    def load_data(self, test_percent=0.15):
+        one_hot = True
+        validation_size = 5000
+
+    def extract_images(self, f):
+        """Extract the images into a 4D uint8 numpy array [index, y, x, depth].
+        Args:
+          f: A file object that can be passed into a gzip reader.
+        Returns:
+          data: A 4D unit8 numpy array [index, y, x, depth].
+        Raises:
+          ValueError: If the bytestream does not start with 2051.
+        """
+        print('Extracting', f.name)
+        with gzip.GzipFile(fileobj=f) as bytestream:
+            magic = self._read32(bytestream)
+            if magic != 2051:
+                raise ValueError('Invalid magic number %d in MNIST image file: %s' % (magic, f.name))
+            num_images = self._read32(bytestream)
+            rows = self._read32(bytestream)
+            cols = self._read32(bytestream)
+            buf = bytestream.read(rows * cols * num_images)
+            data = np.frombuffer(buf, dtype=np.uint8)
+            data = data.reshape(num_images, rows, cols, 1)
+            return data
+
+    def extract_labels(self, f, one_hot=False, num_classes=10):
+        """Extract the labels into a 1D uint8 numpy array [index].
+        Args:
+          f: A file object that can be passed into a gzip reader.
+          one_hot: Does one hot encoding for the result.
+          num_classes: Number of classes for the one hot encoding.
+        Returns:
+          labels: a 1D unit8 numpy array.
+        Raises:
+          ValueError: If the bystream doesn't start with 2049.
+        """
+        print('Extracting', f.name)
+        with gzip.GzipFile(fileobj=f) as bytestream:
+            magic = self._read32(bytestream)
+            if magic != 2049:
+                raise ValueError('Invalid magic number %d in MNIST label file: %s' %
+                                 (magic, f.name))
+            num_items = self._read32(bytestream)
+            buf = bytestream.read(num_items)
+            labels = np.frombuffer(buf, dtype=np.uint8)
+            if one_hot:
+                return self.dense_to_one_hot(labels, num_classes)
+            return labels
+
+    @staticmethod
+    def _read32(bytestream):
+        dt = np.dtype(np.uint32).newbyteorder('>')
+        return np.frombuffer(bytestream.read(4), dtype=dt)[0]

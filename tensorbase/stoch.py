ff801407c7d927f834f3226e7c32194799881621
Dan Salo <dcs41@duke.edu>
2017-02-24 15:10:53 -0500

    convnet and deconvnet

--- tensorbase/stoch.py
+++ tensorbase/stoch.py
@@ -112,7 +112,7 @@ class GaussianLayerConv(StochLayer):
         mu, std, h, w = self.params
         # assert tf.shape(x)[1] == h
         # assert tf.shape(x)[2] == w
-        x_reshape = tf.reshape(x, [self.batch_size, self.eq_samples, self.iw_samples, h, w, self.num_latent])
+        x_reshape = tf.reshape(x, tf.pack([self.batch_size, self.eq_samples, self.iw_samples, h, w, self.num_latent]))
         c = - 0.5 * math.log(2 * math.pi)
         if standard is False:
             density = c - tf.log(std + 1e-10) - (x_reshape - mu) ** 2 / (2 * std**2)

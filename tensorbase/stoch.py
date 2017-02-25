f3da4d8a11eec45a443e973ea60002de3576bb5d
Dan Salo <dcs41@duke.edu>
2017-02-25 16:22:06 -0500

    convnet and deconvnet

--- tensorbase/stoch.py
+++ tensorbase/stoch.py
@@ -105,7 +105,7 @@ class GaussianLayerConv(StochLayer):
         x must be 2D. [batch_size * eqsamples* iwsamples, num_latent]
         """
         mu, std, h, w = self.params
-        shape = tf.pack([32, self.eq_samples, self.iw_samples, x_dims[0], x_dims[1], self.num_latent])
+        shape = tf.pack([32, x_dims[0], x_dims[1], self.num_latent, 1, 1])
         x_reshape = tf.reshape(x, shape)
         c = - 0.5 * math.log(2 * math.pi)
         if standard is False:
